b8d5a12c438ab380fb830d4ddea87c5fef296d5c
sanjukta giri <sanjuktagiri@sanjuktas-MacBook-Air.local>
2026-05-24 18:30:25 +0530

    Counter app implemeted

--- React_Learning_1/src/Components/Counter.jsx
+++ React_Learning_1/src/Components/Counter.jsx
@@ -8,9 +8,19 @@ function Counter(){
 
         }}>
             <h1>Counter : {count}</h1>
-            <button onClick ={()=>{
+            <button style = {{margin : "7px"}} onClick ={()=>{
                 SetCount(count+1);
             }}>Increment</button>
+
+            <button style = {{margin : "7px"}}  onClick = {()=>{
+                SetCount(count-1);
+            }}>Decrement</button>
+
+            <button style = {{margin : "7px"}}  onClick={()=>{
+                SetCount(0);
+            }}>Reset</button>
+
+            {count < 0 && <p>Too Low</p>}
         </div>
 
     );
